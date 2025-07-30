beb112fd8e6b38edc8f600d7eba42c761f5bc85d
unknown <nn0029255@gmail.com>
2025-07-30 21:07:00 +0530

    swaglab2added

---
+++ tests/swaglab2.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+test('standard_user', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/v1/index.html')
+    await page.locator("//input[@data-test='username']").fill('standard_user');
+    await page.locator("//input[@data-test='password']").fill('secret_sauce');
+    await page.locator("//input[@type='submit']").click()
+    await expect(page).toHaveURL('https://www.saucedemo.com/v1/inventory.html')
+});
